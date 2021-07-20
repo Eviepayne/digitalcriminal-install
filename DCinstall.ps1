9fbe4e6e486cbce09cc12c4cb9b42baa6dcc8004
vlad <vlad@vladevpayne.xyz>
2021-07-20 14:35:44 -0400

    removed extra stuff I left in

--- DCinstall.ps1
+++ DCinstall.ps1
@@ -10,8 +10,6 @@ function Get-Loc($name){
 
 function Store-Location($name){
     Get-Location| select path -ExpandProperty path| select path -ExpandProperty path | out-file C:\temp\onlyfans\$name.txt
-    $pth = Get-content C:\temp\onlyfans\$name.txt
-    return $pth
 }
 
 function Test-Location($name){
@@ -32,10 +30,8 @@ function Install-Onlyfans{
 function Install-Packages {
     # install chocolatey
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-    refreshenv
     # Clean up windows dumb shit that gets in the way
     Remove-Item $env:USERPROFILE\AppData\Local\Microsoft\WindowsApps\python*.exe
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
     refreshenv
     # install needed stuff
     choco install git python -y
@@ -48,13 +44,12 @@ function Install-Packages {
 if (!(Test-Path -Path C:\temp\onlyfans)){
     New-Item -Path C:\temp\onlyfans -ItemType directory
 }
-#if (!(Test-Path -Path C:\temp\onlyfans\lastenv.txt)){
+
 if (!(Test-Location("newenv"))){
     ## Are we admin?
     If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
         {
         # Who are we?
-        $env:USERNAME | out-file C:\temp\onlyfans\user.txt
         write-host "We are not admin. Elevating..."
         start-sleep -s 2
         Store-Location("cdir")
@@ -64,7 +59,6 @@ if (!(Test-Location("newenv"))){
         stop-process -Id $PID
         }
 
-    if (!(Test-Path -Path C:\temp\onlyfans)){stop-process -Id $PID}
     $pth = Get-loc("cdir")
     $rep = read-host "`n Everything will go in this folder:`n $pth\OnlyFans`n If you don't want to have anything go in this folder say 'n' if you're ok with this say 'y'.`n You will be able to change where all the content goes, this will only store the script requirements.`n Run from here (y/n)?"
     switch ( $rep )
